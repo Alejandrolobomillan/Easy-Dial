e002d7522b18759546506b19f58c2db0497e2a97
Alejandro Lobo Millan <117042742+Alejandrolobomillan@users.noreply.github.com>
2024-05-14 17:16:21 +0200

    Create phone.cpp

---
+++ phone.cpp
@@ -0,0 +1,5 @@
+//Hem escollit per implementar la classe phone tres paràmetres com especificava a la documentació un natural pel número, un string pel nom i un natural pel nombre de trucades.
+
+nat _numero;
+string _nom;
+nat _trucades;
